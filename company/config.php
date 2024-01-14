4a8f17cdfc049253b524cd4c7bdad262d1728039
hector-teke <hectorguzman422@uma.es>
2024-01-14 12:18:10 +0100

    Initial Commit

--- company/config.php
+++ company/config.php
@@ -4,6 +4,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'password');
 define('DB_NAME', 'companydb');
 
+
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($conn === false)
