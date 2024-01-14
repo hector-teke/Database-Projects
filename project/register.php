2b84ed4f41ebd7dcdc70a304e2b6a9b67b5cdb7f
hector-teke <hectorguzman422@uma.es>
2024-01-14 18:17:06 +0100

    register (no database)

--- project/register.php
+++ project/register.php
@@ -3,40 +3,19 @@ require_once "config.php";
 $passwordIncorrect = false;
 $userIncorrect = false;
 
-// Verify if a form was sent
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $password2 = $_POST["password2"];
+    $description = isset($_POST["description"]) ? $_POST["description"] : null; //description can be empty
 
-    // Prevent SQL inyections
-    $username = mysqli_real_escape_string($conn, $username);
-    $password = mysqli_real_escape_string($conn, $password);
-
-    $query = "SELECT * FROM users WHERE username='$username'";
-
-    if ($result = mysqli_query($conn, $query)) {
-        
-        if (mysqli_num_rows($result) == 1) {
-            $user = mysqli_fetch_assoc($result); //All user information
-
-            // Compare the password
-            if ($password === $user['password']) {
-                $passwordIncorrect = false;
-                $userIncorrect = false;
-
-            } else {
-                $passwordIncorrect = true;
-            }
-        } else {
-            $userIncorrect = true;
-        }
-
+    // Verify passwords are aqual
+    if ($password === $password2) {
+        echo "Registration successful";
     } else {
-        echo "Error: " . mysqli_error($conn);
+        echo "Passwords do not match";
     }
 }
-
-mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -45,25 +24,32 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="login.css"/>
 </head>
 <body>
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="m-4 p-4 rounded shadow bg-light <?php if ($passwordIncorrect || $userIncorrect) echo 'shake';?>">
-            <h2>Login</h2>
+            <h2>Register</h2>
             <form action="" method="post">
                 <div class="mb-3">
                     <label for="username" class="form-label">Username:</label>
-                    <input type="text" class="form-control" id="username" <?php if ($userIncorrect) echo 'placeholder="User not found"';?> name="username" required>
+                    <input type="text" class="form-control" id="username" name="username" required>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password:</label>
-                    <input type="password" class="form-control" id="password" <?php if ($passwordIncorrect) echo 'placeholder="Wrong password"';?> name="password" required>
+                    <input type="password" class="form-control" id="password" name="password" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-                <a href="register.php" class="btn btn-success">Register</a>
+                <div class="mb-3">
+                    <label for="password2" class="form-label">Confirm Password:</label>
+                    <input type="password" class="form-control" id="password2" name="password2" required>
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description: (Optional)</label>
+                    <textarea class="form-control" id="description" name="description" rows="3" placeholder="Tell the people a bit about yourself!"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
             </form>
         </div>
     </div>
