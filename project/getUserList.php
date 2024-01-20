62c6283abfd4349f586e442e14159699d4924339
hector-teke <hectorguzman422@uma.es>
2024-01-20 16:10:29 +0100

    Change user's order to alphabetical order

--- project/getUserList.php
+++ project/getUserList.php
@@ -5,7 +5,7 @@ require_once "config.php";
 $search = isset($_GET['search']) ? $_GET['search'] : '';
 $search = mysqli_real_escape_string($conn, $search);
 
-$query = "SELECT id, username, info FROM users WHERE id != 1 AND username LIKE '%$search%' ORDER BY id DESC LIMIT 15";
+$query = "SELECT id, username, info FROM users WHERE id != 1 AND username LIKE '%$search%' ORDER BY username ASC LIMIT 15";
 $result = mysqli_query($conn, $query);
 
 //Put the results into an array
